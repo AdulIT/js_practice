64df3cc5cad45d7d5b0765cde4f3815c4093a597
AdulIT <spilberg.max@bk.ru>
2021-06-09 15:34:52 +0600

    The practice for loops and conditions

---
+++ js-task_2/js/script.js
@@ -0,0 +1,48 @@
+"use strict";
+
+// Conditions
+// if (4 == 2 + 2) {
+//     console.log('ok');
+// } else {
+//     console.log('not ok');
+// }
+
+// const num = 50;
+// Тернарный оператор ? и :
+// (num === 50) ? console.log('Ok') : console.log('Error');
+
+// switch (num) {
+//     case 49:
+//         console.log('Неверно');
+//         break;
+//     case 100:
+//         console.log('Неверно');
+//         break;
+//     case 51:
+//         console.log('В яблочко!');
+//         break;
+//     default:
+//         console.log('Не в этот раз :((');
+//         break;
+// }
+
+// Loops
+
+// let exp = 50;
+
+// while (exp <= 55) {
+//     console.log(exp);
+//     exp++;
+// }
+
+// do {
+//     console.log(exp);
+//     exp++;
+// }
+// while (exp <= 55);
+
+// for (let i = 1; i < 9; i++) {
+//     console.log(exp);
+//     exp++;
+// }
+
